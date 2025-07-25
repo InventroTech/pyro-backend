3d5a799c2645dc8817f073db32444621c0147607
BIBHAB22 <bibhabindia@gmail.com>
2025-07-25 13:10:56 +0530

    feat: add URL endpoint for get-all-support-tickets

--- src/analytics/urls.py
+++ src/analytics/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path
-from .views import SupabaseAuthCheckView, TicketClosureTimeAnalytics, DailyResolvedTicketsView, DailyPercentileResolutionTimeView ,StackedBarResolvedUnresolvedView
+from .views import SupabaseAuthCheckView, TicketClosureTimeAnalytics, DailyResolvedTicketsView, DailyPercentileResolutionTimeView ,StackedBarResolvedUnresolvedView, GetAllSupportTicketsView
 urlpatterns = [
     path('test-supabase-login/', SupabaseAuthCheckView.as_view(), name = "authcheck"),
     path('ticket-close-time/', TicketClosureTimeAnalytics.as_view(), name = "ticket-close-time"),
     path('tickets/resolved/daily/', DailyResolvedTicketsView.as_view(), name="daily-resolved-tickets"),
     path('tickets/daily-percentile/', DailyPercentileResolutionTimeView.as_view()),
     path('tickets/stacked-bar-daily/', StackedBarResolvedUnresolvedView.as_view(), name = 'stacked-bar'),
+    path('tickets/all/', GetAllSupportTicketsView.as_view(), name="get-all-support-tickets"),
 ]
