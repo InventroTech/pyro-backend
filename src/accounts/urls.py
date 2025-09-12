fe15fbaa01258cd4faa46ce7c80c73a34a2d29d9
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-09-12 19:26:07 +0530

    fix: indexing migration created and fixed unauth error for authz module

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from accounts.views import LegacyUserCreateView, AssigneesByRoleView
+from .views import LinkUserUidView
 
 urlpatterns = [
-    path("users/legacy/create/", LegacyUserCreateView.as_view(), name="legacy-user-create"),
-    path('users/assignees-by-role/', AssigneesByRoleView.as_view(), name='assignees-by-role'),
-    
+    path("link-user-uid/", LinkUserUidView.as_view(), name="link_user_uid"),
 ]
