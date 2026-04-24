6f1c76dc282aca38cb5f174751b981f263b32dc3
Ritam Majumder <ritammajumder0@gmail.com>
2026-04-24 12:59:28 +0530

    slow db query

---
+++ src/crm_records/migrations/0030_add_lead_stats_stage_index.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    # Required for CREATE INDEX CONCURRENTLY.
+    atomic = False
+
+    dependencies = [
+        ("crm_records", "0029_add_queueable_lead_indexes"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+            CREATE INDEX CONCURRENTLY IF NOT EXISTS records_lead_stats_tenant_stage_idx
+            ON public.records (tenant_id, ((data->>'lead_stage')))
+            WHERE entity_type = 'lead';
+            """,
+            reverse_sql="""
+            DROP INDEX CONCURRENTLY IF EXISTS public.records_lead_stats_tenant_stage_idx;
+            """,
+        ),
+    ]
