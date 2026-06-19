139e88852b1b39c4864775498bf8cd308e89e819
Ritam Majumder <ritammajumder0@gmail.com>
2026-06-19 17:56:35 +0530

    test fail

--- src/pyro_jobs/apps.py
+++ src/pyro_jobs/apps.py
@@ -7,6 +7,9 @@ class PyroJobsConfig(AppConfig):
     verbose_name = "Pyro Jobs"
 
     def ready(self):
+        import sys
+        if "pytest" in sys.modules or "test" in sys.argv:
+            return
         if os.environ.get("RUN_MAIN") == "true" or os.environ.get("DJANGO_ENV") != "development":
             from pyro_jobs.brahma import start_brahma
             from pyro_jobs.vishnu import start_vishnu
