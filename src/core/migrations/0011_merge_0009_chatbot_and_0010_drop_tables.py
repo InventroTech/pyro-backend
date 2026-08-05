a0d3f52beaaf953387747b5be2e6e3fa03a2d33d
Harisudhan <harisudhan.nandhu@gmail.com>
2026-08-05 17:15:50 +0530

    Fixed the merge conflict

---
+++ src/core/migrations/0011_merge_0009_chatbot_and_0010_drop_tables.py
@@ -0,0 +1,13 @@
+# Generated manually to resolve conflicting leaf nodes in core.
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0009_chatbot_page_owner_email"),
+        ("core", "0010_drop_tenant_users_and_party"),
+    ]
+
+    operations = []
